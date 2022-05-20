0c0fc6f651f371d0810059941bf6f9ac652fcfb9
agustinTortolero <105939221+agustinTortolero@users.noreply.github.com>
2022-05-20 12:19:40 -0600

    CUDA Impulse Noise Filtering

---
+++ GPU_Experimentos/FuncionesAux.h
@@ -0,0 +1,5 @@
+#pragma once
+void EscribirCriterios(double *valPSNR, double *valMCRE, double *valNCD, double *valMAE, int nExperimentos);
+void ObtenerPath(char* Dir, int i);
+
+void ObtenerPathWrite(char* Dir2, int i);
